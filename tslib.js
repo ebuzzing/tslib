45a531558d9730d0c760246b8448d4e37aae205a
Benoit Ruiz <benoit.ruiz01@gmail.com>
2020-02-28 15:51:21 +0100

    Remove Object.assign from __assign helper definition

--- tslib.js
+++ tslib.js
@@ -69,7 +69,7 @@ var __classPrivateFieldSet;
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 
-    __assign = Object.assign || function (t) {
+    __assign = function (t) {
         for (var s, i = 1, n = arguments.length; i < n; i++) {
             s = arguments[i];
             for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p)) t[p] = s[p];
